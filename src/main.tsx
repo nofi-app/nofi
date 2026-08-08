98d96b0e797393bfcb0ae6d8a29e382bf60aa114
dilludx <55938588+dilludx@users.noreply.github.com>
2026-08-08 22:20:56 +0530

    Add Sentry error monitoring with privacy scrubbing

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.tsx'
+
+const dsn = import.meta.env.VITE_SENTRY_DSN as string | undefined
+if (dsn) {
+  Sentry.init({
+    dsn,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 0.1,
+    sendDefaultPii: false,
+    beforeSend(event) {
+      if (event.request) event.request.headers = undefined
+      return event
+    },
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
