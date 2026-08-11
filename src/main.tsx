e19d97c61dcce652561435ead374e521181c417b
dilludx <55938588+dilludx@users.noreply.github.com>
2026-08-11 17:56:05 +0530

    Apply theme before first paint, add cache headers

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import * as Sentry from '@sentry/react'
 import './index.css'
 import App from './App.tsx'
+import { applyTheme, getTheme } from './lib/theme'
+
+applyTheme(getTheme())
 
 const dsn = import.meta.env.VITE_SENTRY_DSN as string | undefined
 if (dsn) {
